a7016aab29a6fe87137b3ddf978f523f379fb2a3
== <prantocse01@gmail.com>
2022-03-25 00:03:02 +0600

    useref typescript

--- src/components/NewTodo.tsx
+++ src/components/NewTodo.tsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { useRef } from "react";
+
 
 const NewTodo = () => {
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
 
-    const submitHandler = (event: React.FormEvent) => {
-        event.preventDefault();
-    }
-
-
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    const enteredText = todoTextInputRef.current!.value;
+  }
 
   return (
     <form onSubmit={submitHandler}>
       <label htmlFor='text'>Todo text</label>
-      <input type='text' id='text' />
+      <input ref={todoTextInputRef} type='text' id='text' />
       <button>Add Todo</button>
     </form>
   );
