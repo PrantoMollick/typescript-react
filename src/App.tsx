13e922206f9c661c792dcf15e87529ec5f90596a
== <prantocse01@gmail.com>
2022-03-25 00:28:21 +0600

    working with functions props

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
@@ -6,11 +7,14 @@ function App() {
     new Todo('Learn React'), 
     new Todo('Learn TypeScript')
   ];
-  console.log(todos);
   
+  const addTodohandler = (todoText: string) => {
+
+  };
 
   return (
     <div>
+      <NewTodo onAddTodo={addTodohandler} />
       <Todos items={todos} />
     </div>
   );
